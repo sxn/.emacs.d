c89b3f8fb0eba7385586da403bee60be665734b1
Sorin Muntean <me@sorinmuntean.ro>
2021-03-18 11:25:40 +0100

    font size & cursor color

--- config.el
+++ config.el
@@ -20,12 +20,12 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 
-(setq doom-font (font-spec :family "Dank Mono" :size 14)
+(setq doom-font (font-spec :family "Dank Mono" :size 15)
       doom-variable-pitch-font (font-spec :family "Dank Mono")
       ;; There are two ways to load a theme. Both assume the theme is installed and
       ;; available. You can either set `doom-theme' or manually load a theme with the
       ;; `load-theme' function. This is the default:
-      doom-theme 'tsdh-dark
+      doom-theme 'tsdh-light
       ;; If you use `org' and don't want your org files in the default location below,
       ;; change `org-directory'. It must be set before org loads!
       org-directory "~/org/"
@@ -50,6 +50,8 @@
 
       ;; by default, which-key triggers slower than I want
       which-key-idle-delay 0.3)
+
+(set-cursor-color "#FF0000")
 
 ;; Emacs Mac Port binds `<swipe-left>` and `<swipe-right>' to
 ;; `mac-previous-buffer' and `mac-next-buffer'. Let's remove that silliness.
